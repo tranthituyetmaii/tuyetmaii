2d1e9a426cf8877c7367f3baaa518f5ccc3f7af6
tranthituyetmaii <tuyetmai24092005@gmail.com>
2024-08-20 23:21:25 +0700

    Add files via upload

---
+++ buoi6.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 20 20:11:10 2024
+
+@author: Trần Thị Tuyết Mai - 23709551
+Bài 1: Chuỗi
+dia_chi = "Đại học Quốc gia, Khu phố 6, P. Linh Trung, Q. Thủ Đức, Tp.HCM"
+split_1 = ("Đại học Quốc gia")
+split_2 = ("Khu phố 6")
+split_3 = ("P.Linh Trung")
+split_4 = ("Q. Thủ Đức")
+split_5 = ("Tp.HCM")
+len(dia_chi)
+print("", split_1)
+print("", split_2)
+print("", split_3)
+print("", split_4)
+print("", split_5)
+
+Bài 2: Tách chuỗi
+dia_chi = "Đại học Quốc gia, Khu phố 6, P. Linh Trung, Q. Thủ Đức, Tp.HCM"
+var1 = "Đại học Quốc gia"
+var2 = "Khu phố 6"
+var3 = "P. Linh Trung"
+var4 = "Q. Thủ Đức"
+var5 = "Tp. HCM"
+print("", var1[0:15])
+print("", var2[0:9])
+print("", var3[3:13])
+print("", var4[3:10])
+print("", var5[3:7])
+
+Bài 3: im a cat
+input = "iam a cat"
+s1 = input.title()
+print(input.title())
+print(input.upper())
+print(s1.swapcase())
+print(input.lower())
+
+
+
+
+
+"""
+
+
+
